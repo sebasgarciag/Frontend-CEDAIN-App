4470875c472a3643001d14189b2dbed7e85a4908
SalvadoRobles03 <99054999+SalvadoRobles03@users.noreply.github.com>
2023-09-10 23:02:01 -0600

    FrontEnd Structure

---
+++ src/pages/app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+const Home = () => { 
+    return ( 
+    <SafeAreaProvider> 
+        <View> 
+            <Text>Home</Text> 
+        </View> 
+    </SafeAreaProvider> ) }
+
+export default Home;
