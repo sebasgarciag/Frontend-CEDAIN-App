2ef09f65b5f532c6669c0628de5f80d8689374e2
alexis <alexisgargo3@gmail.com>
2023-09-13 14:39:47 -0600

    ced16 added imagenes a los productos

--- src/pages/crearSalida/index.js
+++ src/pages/crearSalida/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
-import { Spacer, HStack, VStack, Box, Flex } from "@react-native-material/core";
+import { Text, ScrollView, Image, Pressable } from "react-native";
+import { Spacer, HStack, VStack, Box } from "@react-native-material/core";
 import { FAB } from "react-native-paper";
 import useSeleccion from "./hookSeleccionProducto";
 
@@ -15,23 +15,24 @@ const SeleccionProductos = () => {
         {categorias.map((categoria) =>
 
 
-            <VStack divider={true} spacing={20} key={categoria}>
+            <VStack spacing={20} key={categoria}>
 
-                    <Text>{categoria}</Text>
+                    <Text style={{fontSize: 20}}>{categoria}</Text>
+
                     <ScrollView horizontal>
                         <HStack divider={true} spacing={10}>
-
                             {productos.filter(producto => producto.categoria === categoria).map((producto) => 
                                 <Box w={96} key={producto.idProducto}>
-                                    <FAB 
-                                        icon="plus"
-                                        size="large"
-                                        onPress={() => console.log('Pressed' + producto.idProducto)}
-                                    />
+
+                                    <Pressable onPress={() => console.log('pressed' + producto.idProducto)}>
+                                        <Image 
+                                            source={require('../../assets/imagenes/ware.jpg')}
+                                            style={{width: 96, height: 96, borderRadius: 10}}
+                                        />
+                                    </Pressable>
                                     <Text>{producto.nombreProducto}</Text>
                                 </Box>)
                             }
-
                         </HStack>
                     </ScrollView>
                 <Spacer />
