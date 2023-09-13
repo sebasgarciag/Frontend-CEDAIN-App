721ca4a689a6d9c9e8b355abe54832c7d2661c0e
TomasO <a01562785@tec.mx>
2023-09-13 11:46:57 -0600

    cambios de comentarios

--- src/components/UI/NavBar.js
+++ src/components/UI/NavBar.js
@@ -5,22 +5,18 @@ import { Appbar, Searchbar } from 'react-native-paper';
 const NavBar = () => {
   return (
     <Appbar.Header style={{ backgroundColor: '#fff' }}>
-      {/* Botón de menú */}
       <Appbar.Action
         icon="menu"
         onPress={() => {
-          // Acción al presionar el botón de menú
         }}
       />
-      
-      {/* Barra de búsqueda */}
       <Searchbar
         style={{
           flex: 1,
           marginLeft: 10,
-          height: 40,  // Ajuste de altura
-          borderRadius: 15,  // Hace las esquinas redondeadas
-          paddingVertical: -6  // Ajuste de padding vertical
+          height: 40,  
+          borderRadius: 15,  
+          paddingVertical: -6  
         }}
         placeholder="Buscar"
       />
