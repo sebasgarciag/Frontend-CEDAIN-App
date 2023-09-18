bfc4fbd1607527604e0f2bc654e9fb14dfa335c0
TomasO <a01562785@tec.mx>
2023-09-18 16:35:37 -0600

    dorpdown

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ import { View, Text } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import NavBar from "../src/components/UI/NavBar";
 import BotonASC from "../src/components/UI/BotonASC";
+import DropdownC from "../src/components/UI/dropDownC";
+import DropdownE from "../src/components/UI/dropDownE";
 
 
 const Home = () => { 
@@ -9,6 +11,8 @@ const Home = () => {
     <SafeAreaProvider> 
         <View> 
             <NavBar/>
+            <DropdownC/>
+            <DropdownE/>
             <BotonASC 
                 textoBoton1="Entrada" 
                 textoBoton2="Salida" 
