9795f5db3c28de01364f57fd07cf30e974000b9c
sebasgarciag <sebastiangrciag@gmail.com>
2023-09-21 15:54:31 -0600

    Pantallas agregadas a pagina de botones de equipo

--- app/index.js
+++ app/index.js
@@ -1,9 +1,12 @@
+import React, { useState } from 'react';
 import { View, Text, Pressable } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { router } from "expo-router";
 import { Button, Stack } from "@react-native-material/core";
 
 const Home = () => { 
+    const [showAdiseButtons, setShowAdiseButtons] = useState(false);
+
     return ( 
     <SafeAreaProvider> 
         <Stack fill center spacing={20}>
@@ -20,8 +23,46 @@ const Home = () => {
             />
             <Button
                 title="adise"
-                onPress={() => router.replace("/")}
+                onPress={() => setShowAdiseButtons(!showAdiseButtons)}
             />
+            
+            {showAdiseButtons && (
+                <>
+                    <Button
+                        title="adise - entradaAlm"
+                        onPress={() => router.replace("/entradaAlm")}
+                    />
+                    <Button
+                        title="adise - entradaAdmin"
+                        onPress={() => router.replace("/entradaAdmin")}
+                    />
+                    <Button
+                        title="adise - listadoEntradasAdmin"
+                        onPress={() => router.replace("/listadoEntradasAdmin")}
+                    />
+                    <Button
+                        title="adise - listadoEntradasAlm"
+                        onPress={() => router.replace("/listadoEntradasAlm")}
+                    />
+                    <Button
+                        title="adise - listadoSalidasAdmin"
+                        onPress={() => router.replace("/listadoSalidasAdmin")}
+                    />
+                    <Button
+                        title="adise - listadoSalidasAlm"
+                        onPress={() => router.replace("/listadoSalidasAlm")}
+                    />
+                    <Button
+                        title="adise - salidaAdmin"
+                        onPress={() => router.replace("/salidaAdmin")}
+                    />
+                    <Button
+                        title="adise - salidaAlm"
+                        onPress={() => router.replace("/salidaAlm")}
+                    />
+                </>
+            )}
+
             <Button
                 title="ced"
                 onPress={() => router.replace("/resumenSalida")}
