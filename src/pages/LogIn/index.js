94ec33cf2a935e1ce026e7dc5fbfd325d7b28c1c
SalvadoRobles03 <99054999+SalvadoRobles03@users.noreply.github.com>
2023-10-01 22:00:42 -0600

    Ya jala login pero con ip local

--- src/pages/LogIn/index.js
+++ src/pages/LogIn/index.js
@@ -10,11 +10,12 @@ import UsuariosAPI from '../../apis/usuariosApi';
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
- 
+  const [borderColor, setBorderColor] = useState('normal');
+
 
   const validateEmail = (email) => {
-      const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-      return regex.test(email);
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
   }
 
   const handleRegister = () => {
@@ -32,91 +33,92 @@ const LoginPage = () => {
     UsuariosAPI().login(email, password)
       .then(function (data) {
         // La autenticación es exitosa
-        console.log("Inicio de sesión exitoso");
+        console.log("Connected");
         console.log(data);
+        setBorderColor('normal'); // Restablece el color del borde a normal después de un inicio de sesión exitoso
       })
       .catch(function (error) {
         // La autenticación falla
         console.error(error);
         if (error.message === 'Network Error') {
           console.log("No se pudo conectar a la base de datos");
-          Alert.alert("Error", "No se pudo conectar a la base de datos");
-        } else {
-          Alert.alert("Error", "Correo o contraseña incorrectos");
+        } else if (error.response.status === 401) {
+          setBorderColor('red'); // Cambia el color del borde de los inputs a rojo
         }
       });
 
 
+
   }
-  
+
 
   return (
-      <ScrollView style={styles.Container}>
-          <Title style={styles.Title}>Iniciar sesión</Title>
-          <TextInput
-              label="Email"
-              value={email}
-              onChangeText={text => setEmail(text)}
-              style={styles.Input}
-          />
-          <TextInput
-              secureTextEntry={true}
-              label="Password"
-              value={password}
-              onChangeText={text => setPassword(text)}
-              style={styles.Input}
-          />
-          
+    <ScrollView style={styles.Container}>
+      <Title style={styles.Title}>Iniciar sesión</Title>
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={text => setEmail(text)}
+        style={styles.Input}
+      />
+      <TextInput
+        secureTextEntry={true}
+        label="Password"
+        value={password}
+        onChangeText={text => setPassword(text)}
+        style={styles.Input}
+      />
 
-          <TouchableOpacity onPress={handleRegister} style={styles.Button}>
-              <Icon name="check-circle" size={24} color="#F1EFE3" style={{ marginRight: 10 }}></Icon>
-              <Text style={{ fontSize: 18, color: '#F1EFE3', textAlign: 'center' }}>Iniciar Sesion</Text>
-          </TouchableOpacity>
-          {/* <VolverButton onPress={() => { router.replace("/botonesChernobyl"); }} /> */}
-          <VolverButton ruta="/botonesChernobyl" />
 
-      </ScrollView>
+      <TouchableOpacity onPress={handleRegister} style={styles.Button}>
+        <Icon name="check-circle" size={24} color="#F1EFE3" style={{ marginRight: 10 }}></Icon>
+        <Text style={{ fontSize: 18, color: '#F1EFE3', textAlign: 'center' }}>Iniciar Sesion</Text>
+      </TouchableOpacity>
+      {/* <VolverButton onPress={() => { router.replace("/botonesChernobyl"); }} /> */}
+      <VolverButton ruta="/botonesChernobyl" />
+
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
   Container: {
-      display: 'flex',
-      backgroundColor: '#F1EFE3',
-      paddingTop: '30%',
-      
+    display: 'flex',
+    backgroundColor: '#F1EFE3',
+    paddingTop: '30%',
+
   },
   Title: {
-      fontSize: 36,
-      fontWeight: 'bold',
-      color: '#333',
-      textAlign: 'center',
-      marginBottom: 20,
-      paddingTop:20,
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center',
+    marginBottom: 20,
+    paddingTop: 20,
 
   },
   Input: {
-      backgroundColor: '#fff',
-      borderRadius: 10,
-      marginVertical: 10,
-      maxWidth: '90%',
-      marginLeft: '5%',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    marginVertical: 10,
+    maxWidth: '90%',
+    marginLeft: '5%',
   },
   Button: {
-      marginTop:20,
-      justifyContent: 'center',
-      textAlign: 'center',
-      backgroundColor: '#59CD90',
-      paddingVertical: 15,
-      paddingHorizontal: 40,
-      borderRadius: 30,
-      flexDirection: 'row',
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.23,
-      shadowRadius: 2.62,
-      elevation: 4,
-      alignSelf: 'center',
+    marginTop: 20,
+    justifyContent: 'center',
+    textAlign: 'center',
+    backgroundColor: '#59CD90',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 30,
+    flexDirection: 'row',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+    alignSelf: 'center',
   },
 });
 
