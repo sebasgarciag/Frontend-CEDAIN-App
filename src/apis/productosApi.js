73dfb3ab3a3fb9fcd25b55c171a8da90a6ef568e
TomasO <a01562785@tec.mx>
2023-10-05 20:31:23 -0600

    CED-66

--- src/apis/productosApi.js
+++ src/apis/productosApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import ip from './ipApi';
 
-const baseUrl = `http://${ip}:8080`;
+const baseUrl = `https://${ip}`;
 
 const ProductosAPI = () => {
     async function getTodosProductos() {
