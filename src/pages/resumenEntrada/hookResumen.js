c75ac79adfc038a8d6f205ac113ffe1470d0f8b2
alexis <alexisgargo3@gmail.com>
2023-10-06 03:43:19 -0600

    ced fixed crear salida

--- src/pages/resumenEntrada/hookResumen.js
+++ src/pages/resumenEntrada/hookResumen.js
@@ -34,59 +34,6 @@ const useResumen = () => {
 
     const [carrito, setCarrito] = useState([]);
 
-    // const [datosEntrada, setDatosEntrada] = useState([
-    //     {
-    //         producto: "producto1",
-    //         cantidad: 0,    
-    //         precio: 1,
-    //         tamano: "mediano",
-    //         foto: "../../assets/imagenes/ware.jpg"
-    //     },
-    //     {
-    //         producto: "producto2",
-    //         cantidad: 0,
-    //         precio: 2,
-    //         tamano: "grande",
-    //         foto: "../../assets/imagenes/ware.jpg"
-    //     },
-    //     {
-    //         producto: "producto3",
-    //         cantidad: 0,
-    //         precio: 3,
-    //         tamano: "chico",
-    //         foto: "../../assets/imagenes/ware.jpg"
-    //     },
-    //     {
-    //         producto: "producto4",
-    //         cantidad: 0,
-    //         precio: 4,
-    //         tamano: "mediano",
-    //         foto: "../../assets/imagenes/ware.jpg"
-    //     },
-    //     {
-    //         producto: "producto5",
-    //         cantidad: 0,
-    //         precio: 5,
-    //         tamano: "grande",
-    //         foto: "../../assets/imagenes/ware.jpg"
-    //     }
-    
-    // ]);
-
-    function handleCantidad(producto, newCantidad) {
-        const newDatos = datosEntrada.map((datosEntrada) => {
-            if (datosEntrada.producto === producto) {
-                if (isNaN(parseInt(newCantidad))) {
-                    newCantidad = 0;
-                }
-                return { ...datosEntrada, cantidad: parseInt(newCantidad) };
-            } else {
-                return datosEntrada;
-            }
-        });
-        setDatosEntrada(newDatos);
-    }
-
 
     async function terminar(entrada, detallesEntrada) {
         const date = new Date();
@@ -129,7 +76,7 @@ const useResumen = () => {
         }
     }
 
-    return { entrada, setEntrada, handleCantidad, carrito, setCarrito, terminar };
+    return { entrada, setEntrada, carrito, setCarrito, terminar };
 }
 
 export default useResumen;
