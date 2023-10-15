c63ba2634d86b5123a2c066819efd0df6c5c93ff
Nacho <A00756853@tec.mx>
2023-10-15 11:29:03 -0600

    Se trabajo en pruebas de e2e

--- __tests__/postEntrada.test.js
+++ __tests__/postEntrada.test.js
@@ -7,9 +7,15 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react-nativ
 import CrearEntrada from "../app/crearEntrada";
 import Inventario2 from "../app/inventario";
 import CarritoEntrada from "../app/carritoEntrada";
-import InfoDestinoEntrada from "../app/infoDestinoEntrada"
+import InfoDestinoEntrada from "../app/infoDestinoEntrada";
+import ResumenEntrada from "../app/ResumenEntrada";
 
 const Stack = createNativeStackNavigator();
+
+jest.mock('@react-navigation/native-stack', () => ({
+    ...jest.requireActual('@react-navigation/native-stack'),
+    createNativeStackNavigator: jest.fn(),
+  }));
 
 jest.mock('@react-navigation/native', () => ({
     ...jest.requireActual('@react-navigation/native'),
@@ -20,27 +26,34 @@ jest.mock('@react-navigation/native', () => ({
     }),
 }));
 
+
 //POSTS
 function InventarioScreen() {
-    reutnr (
+    return (
         <Inventario2/>
     );
 }
 function CrearEntradaScreen() {
-    reutnr (
+    return (
         <CrearEntrada/>
     );
 }
 function CarritoEntradaScreen() {
-    reutnr (
+    return (
         <CarritoEntrada/>
     );
 }
 function InfoDestinoEntradaScreen() {
-    reutnr (
+    return (
         <InfoDestinoEntrada/>
     );
 }
+function ResumenEntradaScreen() {
+    return (
+        <ResumenEntrada/>
+    );
+}
+
 
 describe('Pruebas de Crear Entrada', () => {
     test('Given required info to create an entrada, creats a new entrada.', async () => {
@@ -51,8 +64,6 @@ describe('Pruebas de Crear Entrada', () => {
         const id_comunidad = 1;
         const id_evento = 1;
 
-
-
         await render(
             <NavigationContainer>
                 <Stack.Navigator>
@@ -61,40 +72,65 @@ describe('Pruebas de Crear Entrada', () => {
             </NavigationContainer>
         );
 
-        await fireEvent.press(screen.getByText("Entrada Nueva"));
+        console.log("================================")
+        // await fireEvent.press(screen.getByText("Entrada Nueva"));
 
-        await render(
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name ="/CrearEntrada" component={CrearEntradaScreen} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        );
+        // await render(
+        //     <NavigationContainer>
+        //         <Stack.Navigator>
+        //             <Stack.Screen name ="/CrearEntrada" component={CrearEntradaScreen} />
+        //         </Stack.Navigator>
+        //     </NavigationContainer>
+        // );
 
-        await fireEvent.press(screen.getByText("Canasta de barro"));
-        await fireEvent.press(screen.getByText("Siguiente"));
+        // await fireEvent.press(screen.getByText("Canasta de barro"));
+        // await fireEvent.press(screen.getByText("Siguiente"));
 
-        await render(
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name ="/carritoEntrada" component={CarritoEntradaScreen} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        );
+        // await render(
+        //     <NavigationContainer>
+        //         <Stack.Navigator>
+        //             <Stack.Screen name ="/carritoEntrada" component={CarritoEntradaScreen} />
+        //         </Stack.Navigator>
+        //     </NavigationContainer>
+        // );
 
-        await fireEvent.press(screen.getByText("+"));
-        await fireEvent.press(screen.getByText("Siguiente"));
+        // await fireEvent.press(screen.getByText("+"));
+        // await fireEvent.press(screen.getByText("Siguiente"));
 
-        await render(
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name ="/InfoDestinoEntrada" component={InfoDestinoEntradaScreen} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        );
+        // await render(
+        //     <NavigationContainer>
+        //         <Stack.Navigator>
+        //             <Stack.Screen name ="/InfoDestinoEntrada" component={InfoDestinoEntradaScreen} />
+        //         </Stack.Navigator>
+        //     </NavigationContainer>
+        // );
 
-        await fireEvent.changeText(screen.getByLabelText('Observaciones'), observaciones);
-        await fireEvent.changeText(screen.getByLabelText('Emisor'), emisor);
-        await fireEvent.press(screen.getByText("Siguiente"));
+        // await fireEvent.press(screen.getByText("Comunidad"));
+        // await fireEvent.press(screen.getByText("No Aplica"));
+        // await fireEvent.press(screen.getByText("Comunidad"));
+        // await fireEvent.press(screen.getByText("No Aplica"));
 
-        
+        // await fireEvent.press(screen.getByText("Evento"));
+        // await fireEvent.press(screen.getByText("No Aplica"));
+        // await fireEvent.press(screen.getByText("Evento"));
+        // await fireEvent.press(screen.getByText("No Aplica"));
+
+        // await fireEvent.changeText(screen.getByLabelText('Observaciones'), observaciones);
+        // await fireEvent.changeText(screen.getByLabelText('Emisor'), emisor);
+        // await fireEvent.press(screen.getByText("Siguiente"));
+
+        // await render(
+        //     <NavigationContainer>
+        //         <Stack.Navigator>
+        //             <Stack.Screen name ="/ResumenEntrada" component={ResumenEntradaScreen} />
+        //         </Stack.Navigator>
+        //     </NavigationContainer>
+        // );
+
+        // await fireEvent.press(screen.getByText("Terminar"));
+
+        // await waitFor(() => {
+        //     expect(screen.getByText(expectedOutput)).toBeTruthy();
+        // }, { timeout: 5000 });
+    })
+})
