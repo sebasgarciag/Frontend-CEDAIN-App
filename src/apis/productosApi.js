c12cbe3dfb3f3383ad027d3f379744d936de93d9
Angel Corrales <corralessoteloangel@outlook.com>
2023-10-18 16:56:03 -0600

    js docs for productos

--- src/apis/productosApi.js
+++ src/apis/productosApi.js
@@ -6,14 +6,19 @@ const baseUrl = `${protocol}${ip}:${port}`;
 //const baseUrl = `http://${ip}:8080`;
 //const baseUrl = `https://${ip}`;
 
+
 const ProductosAPI = () => {
+    /**
+     * Obtiene todos los productos desde la API.
+     * @async
+     * @function
+     * @returns {Promise} Promesa que resuelve en la respuesta de la API.
+     */
     async function getTodosProductos() {
         let response = null;
 
         try {
-            // console.log(ip)
             response = await axios.get(`${baseUrl}/productos`);
-            // console.log(response.data)
         } catch (error) {
             console.log("Error: " + error);
         }
@@ -21,13 +26,17 @@ const ProductosAPI = () => {
         return response;
     }
 
+    /**
+     * Obtiene todos los tamaños de productos desde la API.
+     * @async
+     * @function
+     * @returns {Promise} Promesa que resuelve en la respuesta de la API.
+     */
     async function getTodosTamanios() {
         let response = null;
 
         try {
-            // console.log(ip)
             response = await axios.get(`${baseUrl}/productos/tamanios/todos`);
-            // console.log(response.data)
         } catch (error) {
             console.log("Error: " + error);
         }
@@ -35,13 +44,17 @@ const ProductosAPI = () => {
         return response;
     }
 
+    /**
+     * Obtiene todas las categorías de productos desde la API.
+     * @async
+     * @function
+     * @returns {Promise} Promesa que resuelve en la respuesta de la API.
+     */
     async function getTodasCategorias() {
         let response = null;
 
         try {
-            // console.log(ip)
             response = await axios.get(`${baseUrl}/productos/categorias/todas`);
-            // console.log(response.data)
         } catch (error) {
             console.log("Error: " + error);
         }
@@ -49,7 +62,15 @@ const ProductosAPI = () => {
         return response;
     }
 
+    /**
+     * Crea un nuevo producto utilizando los datos proporcionados en el formulario.
+     * @async
+     * @function
+     * @param {FormData} formData - Datos del formulario para el nuevo producto.
+     * @returns {Promise} Promesa que resuelve en la respuesta de la API.
+     */
     async function createProducto(formData) {
+        let response = null;
 
         try {
             response = await axios.post(`${baseUrl}/productos`, formData, {
@@ -60,41 +81,57 @@ const ProductosAPI = () => {
             console.log(response.data);
         } catch (error) {
             console.log("Error: " + error);
-            // Si el error es un error de red (lo que podría indicar un problema de conexión con la base de datos)
+
             if (axios.isAxiosError(error) && !error.response) {
                 console.log("No se pudo conectar a la base de datos");
-                Alert.alert("Error", "No se pudo conectar a la base de datos");
+                // Alert.alert("Error", "No se pudo conectar a la base de datos");
             }
         }
 
         return response;
     }
+
+    /**
+     * Actualiza un producto con una imagen adjunta.
+     * @async
+     * @function
+     * @param {FormData} formData - Datos del formulario con la imagen a subir.
+     * @param {string} id_producto - Identificador del producto a actualizar.
+     * @returns {Promise} Promesa que resuelve en la respuesta de la API.
+     */
     async function updateProductoConImagen(formData, id_producto) {
         let response = null;
 
         try {
-            const response = await axios.put(`${baseUrl}/productos/${id_producto}`, formData, {
+            response = await axios.put(`${baseUrl}/productos/${id_producto}`, formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
             });
             console.log(response.data);
         } catch (err) {
-            console.error("Error uploading image:", err);
+            console.error("Error al cargar la imagen:", err);
         }
 
         return response;
-    };
-    
+    }
+
+    /**
+     * Obtiene un producto específico desde la API.
+     * @async
+     * @function
+     * @param {string} id_producto - Identificador del producto a obtener.
+     * @returns {Promise} Promesa que resuelve en la respuesta de la API.
+     */
     async function getProducto(id_producto) {
         let response = null;
 
         try {
-            response = await axios.get(`${baseUrl}/productos/${id_producto}`)
+            response = await axios.get(`${baseUrl}/productos/${id_producto}`);
         } catch (error) {
             if (error.response) {
                 if (error.response.status === 400) {
-                    console.log("Invalid Parameter");
+                    console.log("Parámetro inválido");
                 }
             }
 
